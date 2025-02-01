2f6b6e896f0d9c1b2b957ec09e5b6b6381e513d7
AlejandroPinto99 <a.pinto8899@gmail.com>
2025-02-01 12:16:37 -0600

    Adding native names

--- src/views/Details.jsx
+++ src/views/Details.jsx
@@ -29,7 +29,6 @@ const Details = () => {
     , [name]);
 
     const getStringData = (array) => {
-        console.log({ array })
         if(array.length === 1) return array[0];
 
         const stringData = array.join(", ");
@@ -37,6 +36,7 @@ const Details = () => {
         return stringData;
     }
 
+    console.log({ countryInfo })
     return (
         <div className={`${theme === 'dark' ? 'bg-[#202C36]' : 'bg-[#fafafa]'}`}>
             {
@@ -54,7 +54,7 @@ const Details = () => {
                             <div className={`md:grid md:grid-cols-2 md:gap-8 md:mt-4 ${theme === "light" ? 'text-black' : 'text-white'}`}>
                                 <h2 className="font-extrabold  text-[22px] md:col-span-2">{countryInfo.name?.common}</h2>
                                 <div className="my-4 ">
-                                    <p className="font-semibold">Native Name: <span className="font-light">Belgie</span></p>
+                                    <p className="font-semibold">Native Name: <span className="font-light">{getStringData(Object.values(countryInfo?.name.nativeName)?.map(item => item.common))}</span></p>
                                     <p className="font-semibold">Population: <span className="font-light">{countryInfo.population}</span></p>
                                     <p className="font-semibold">Region: <span className="font-light">{countryInfo.region}</span></p>
                                     <p className="font-semibold">Sub Region: <span className="font-light">{countryInfo.subregion}</span></p>
